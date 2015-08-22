1defd4350c48feee4ca94bb106df59a8c5ed02e0
r7kamura <r7kamura@gmail.com>
2015-08-23 00:04:36 +0900

    Add webview example

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # retro-twitter-client
 A retro twitter client.
 
-![](/images/preview.png)
+![](/images/preview1.png)
 
 ## Development
 ### npm run setup
--- main.js
+++ main.js
@@ -12,7 +12,7 @@ app.on('window-all-closed', function () {
 });
 
 app.on('ready', function () {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({ width: 1200, height: 800 });
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
   mainWindow.on('closed', function () {
     mainWindow = null;
