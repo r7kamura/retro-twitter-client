b3585f3241f42daf42530c985c75e4fe8640dea0
r7kamura <r7kamura@gmail.com>
2015-08-23 14:18:11 +0900

    Move all app domain logic form main.js into Application class

---
+++ src/application.js
@@ -0,0 +1,62 @@
+import app from 'app'
+import BrowserWindow from 'browser-window'
+import crashReporter from 'crash-reporter'
+import Twitter from 'node-twitter-api'
+
+export default class Application {
+  constructor() {
+    this.accessToken = null;
+    this.accessTokenSecret = null;
+    this.mainWindow = null;
+    this.startCrashReporter();
+    this.registerApplicationCallbacks();
+  }
+
+  registerApplicationCallbacks() {
+    app.on('window-all-closed', () => {
+      if (process.platform != 'darwin') {
+        app.quit();
+      }
+    });
+
+    app.on('ready', () => {
+      const twitter = new Twitter({
+        callback: 'http://example.com',
+        consumerKey: 'KAR2eM09o2GCddFfHUXz7vFKV',
+        consumerSecret: '8MoozYzEzkstemW4fagnm5qlGMVELIxuWBTcBOz0BpUDIpDWqY'
+      });
+
+      twitter.getRequestToken((error, requestToken, requestTokenSecret) => {
+        const url = twitter.getAuthUrl(requestToken);
+        const authenticationWindow = new BrowserWindow({ width: 800, height: 600 });
+        authenticationWindow.webContents.on('will-navigate', (event, url) => {
+          let matched;
+          if (matched = url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/)) {
+            twitter.getAccessToken(requestToken, requestTokenSecret, matched[2], (error, accessToken, accessTokenSecret) => {
+              this.accessToken = accessToken;
+              this.accessTokenSecret = accessTokenSecret;
+            });
+          }
+          event.preventDefault();
+          this.openMainWindow();
+          setTimeout(() => {
+            authenticationWindow.close();
+          }, 0);
+        });
+        authenticationWindow.loadUrl(url);
+      });
+    });
+  }
+
+  openMainWindow() {
+    this.mainWindow = new BrowserWindow({ width: 1200, height: 800 });
+    this.mainWindow.loadUrl(`file://${__dirname}/../public/index.html`);
+    this.mainWindow.on('closed', () => {
+      this.mainWindow = null;
+    });
+  }
+
+  startCrashReporter() {
+    crashReporter.start();
+  }
+}
