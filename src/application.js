33d0bf7be12d891daff3c02413e04a68dd77699e
r7kamura <r7kamura@gmail.com>
2015-08-23 15:07:45 +0900

    Create AuthenticationWindow class

--- src/application.js
+++ src/application.js
@@ -1,8 +1,7 @@
 import app from 'app'
-import BrowserWindow from 'browser-window'
+import AuthenticationWindow from './authentication-window'
 import crashReporter from 'crash-reporter'
 import MainWindow from './main-window'
-import Twitter from 'node-twitter-api'
 
 export default class Application {
   constructor() {
@@ -20,32 +19,9 @@ export default class Application {
         app.quit();
       }
     });
-
     app.on('ready', () => {
-      const twitter = new Twitter({
-        callback: 'http://example.com',
-        consumerKey: 'KAR2eM09o2GCddFfHUXz7vFKV',
-        consumerSecret: '8MoozYzEzkstemW4fagnm5qlGMVELIxuWBTcBOz0BpUDIpDWqY'
-      });
-
-      twitter.getRequestToken((error, requestToken, requestTokenSecret) => {
-        const url = twitter.getAuthUrl(requestToken);
-        const authenticationWindow = new BrowserWindow({ width: 800, height: 600 });
-        authenticationWindow.webContents.on('will-navigate', (event, url) => {
-          let matched;
-          if (matched = url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/)) {
-            twitter.getAccessToken(requestToken, requestTokenSecret, matched[2], (error, accessToken, accessTokenSecret) => {
-              this.accessToken = accessToken;
-              this.accessTokenSecret = accessTokenSecret;
-            });
-          }
-          event.preventDefault();
-          this.openMainWindow();
-          setTimeout(() => {
-            authenticationWindow.close();
-          }, 0);
-        });
-        authenticationWindow.loadUrl(url);
+      new AuthenticationWindow(() => {
+        this.openMainWindow();
       });
     });
   }
---
+++ src/authentication-window.js
@@ -0,0 +1,36 @@
+import BrowserWindow from 'browser-window'
+import Twitter from 'node-twitter-api'
+
+export default class AuthenticationWindow {
+  /**
+   * @param {Object} twitter
+   * @param {String} url
+   */
+  constructor(callback) {
+    const twitter = new Twitter({
+      callback: 'http://example.com',
+      consumerKey: 'KAR2eM09o2GCddFfHUXz7vFKV',
+      consumerSecret: '8MoozYzEzkstemW4fagnm5qlGMVELIxuWBTcBOz0BpUDIpDWqY'
+    });
+
+    twitter.getRequestToken((error, requestToken, requestTokenSecret) => {
+      const url = twitter.getAuthUrl(requestToken);
+      this.window = new BrowserWindow({ width: 800, height: 600 });
+      this.window.webContents.on('will-navigate', (event, url) => {
+        let matched;
+        if (matched = url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/)) {
+          twitter.getAccessToken(requestToken, requestTokenSecret, matched[2], (error, accessToken, accessTokenSecret) => {
+            this.accessToken = accessToken;
+            this.accessTokenSecret = accessTokenSecret;
+          });
+        }
+        event.preventDefault();
+        callback();
+        setTimeout(() => {
+          this.window.close();
+        }, 0);
+      });
+      this.window.loadUrl(url);
+    });
+  }
+}
