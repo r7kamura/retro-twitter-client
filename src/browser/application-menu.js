bf4083160ffb2467af7266f663a1d8050daa2392
r7kamura <r7kamura@gmail.com>
2015-09-10 00:06:33 +0900

    Refactor menu code

---
+++ src/browser/application-menu.js
@@ -0,0 +1,114 @@
+import EventEmitter from 'events'
+import Menu from 'menu'
+
+export default class ApplicationMenu extends EventEmitter {
+  constructor() {
+    super();
+    const self = this;
+    Menu.setApplicationMenu(
+      Menu.buildFromTemplate(
+        [
+          {
+            label: 'Application',
+            submenu: [
+              {
+                label: 'About',
+                click() {
+                  openExternal('https://github.com/r7kamura/retro-twitter-client');
+                }
+              },
+              {
+                type: 'separator'
+              },
+              {
+                label: 'Quit',
+                accelerator: 'Command+Q',
+                click() {
+                  self.emit('quit');
+                }
+              }
+            ]
+          },
+          {
+            label: 'Edit',
+            submenu: [
+              {
+                label: 'Undo',
+                accelerator: 'Command+Z',
+                selector: 'undo:'
+              },
+              {
+                label: 'Redo',
+                accelerator: 'Shift+Command+Z',
+                selector: 'redo:'
+              },
+              {
+                type: 'separator'
+              },
+              {
+                label: 'Cut',
+                accelerator: 'Command+X',
+                selector: 'cut:'
+              },
+              {
+                label: 'Copy',
+                accelerator: 'Command+C',
+                selector: 'copy:'
+              },
+              {
+                label: 'Paste',
+                accelerator: 'Command+V',
+                selector: 'paste:'
+              },
+              {
+                label: 'Select All',
+                accelerator: 'Command+A',
+                selector: 'selectAll:'
+              }
+            ]
+          },
+          {
+            label: 'View',
+            submenu: [
+              {
+                label: 'Search',
+                accelerator: 'Command+F',
+                click() {
+                  self.emit('search')
+                }
+              },
+              {
+                label: 'Select next channel',
+                accelerator: 'Alt+Down',
+                click() {
+                  self.emit('select-next-channel');
+                }
+              },
+              {
+                label: 'Select previous channel',
+                accelerator: 'Alt+Up',
+                click() {
+                  self.emit('select-previous-channel');
+                }
+              },
+              {
+                label: 'Reload',
+                accelerator: 'Command+R',
+                click() {
+                  self.emit('reload');
+                }
+              },
+              {
+                label: 'Open DevTools',
+                accelerator: 'Alt+Command+I',
+                click() {
+                  self.emit('open-dev-tools');
+                }
+              }
+            ]
+          }
+        ]
+      )
+    );
+  }
+}
--- src/browser/application.js
+++ src/browser/application.js
@@ -1,10 +1,10 @@
 import { openExternal } from 'shell'
 import app from 'app'
+import ApplicationMenu from './application-menu'
 import AuthenticationWindow from './authentication-window'
 import BrowserWindow from 'browser-window'
 import crashReporter from 'crash-reporter'
 import MainWindow from './main-window'
-import Menu from 'menu'
 
 export default class Application {
   constructor() {
@@ -63,112 +63,19 @@ export default class Application {
   }
 
   setApplicationMenu() {
-    const self = this;
-    Menu.setApplicationMenu(
-      Menu.buildFromTemplate(
-        [
-          {
-            label: 'Application',
-            submenu: [
-              {
-                label: 'About',
-                click() {
-                  openExternal('https://github.com/r7kamura/retro-twitter-client');
-                }
-              },
-              {
-                type: 'separator'
-              },
-              {
-                label: 'Quit',
-                accelerator: 'Command+Q',
-                click() {
-                  app.quit();
-                }
-              }
-            ]
-          },
-          {
-            label: 'Edit',
-            submenu: [
-              {
-                label: 'Undo',
-                accelerator: 'Command+Z',
-                selector: 'undo:'
-              },
-              {
-                label: 'Redo',
-                accelerator: 'Shift+Command+Z',
-                selector: 'redo:'
-              },
-              {
-                type: 'separator'
-              },
-              {
-                label: 'Cut',
-                accelerator: 'Command+X',
-                selector: 'cut:'
-              },
-              {
-                label: 'Copy',
-                accelerator: 'Command+C',
-                selector: 'copy:'
-              },
-              {
-                label: 'Paste',
-                accelerator: 'Command+V',
-                selector: 'paste:'
-              },
-              {
-                label: 'Select All',
-                accelerator: 'Command+A',
-                selector: 'selectAll:'
-              }
-            ]
-          },
-          {
-            label: 'View',
-            submenu: [
-              {
-                label: 'Search',
-                accelerator: 'Command+F',
-                click() {
-                  self.focusSearchBox();
-                }
-              },
-              {
-                label: 'Select next channel',
-                accelerator: 'Alt+Down',
-                click() {
-                  self.selectNextChannel();
-                }
-              },
-              {
-                label: 'Select previous channel',
-                accelerator: 'Alt+Up',
-                click() {
-                  self.selectPreviousChannel();
-                }
-              },
-              {
-                label: 'Reload',
-                accelerator: 'Command+R',
-                click() {
-                  BrowserWindow.getFocusedWindow().reloadIgnoringCache();
-                }
-              },
-              {
-                label: 'Open DevTools',
-                accelerator: 'Alt+Command+I',
-                click() {
-                  BrowserWindow.getFocusedWindow().toggleDevTools();
-                }
-              }
-            ]
-          }
-        ]
-      )
-    );
+    new ApplicationMenu().on('open-dev-tools', () => {
+      this.mainWindow.window.toggleDevTools();
+    }).on('quit', () => {
+      app.quit();
+    }).on('reload', () => {
+      this.mainWindow.window.reloadIgnoringCache();
+    }).on('search', () => {
+      this.focusSearchBox();
+    }).on('select-next-channel', () => {
+      this.selectNextChannel();
+    }).on('select-previous-channel', () => {
+      this.selectPreviousChannel();
+    });
   }
 
   startCrashReporter() {
